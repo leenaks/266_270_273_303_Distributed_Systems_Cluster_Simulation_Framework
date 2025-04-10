1b1e402e56190f5e00c3fd47290cbe5502710ac8
leenaks <leenakallambella2004@gmail.com>
2025-04-10 10:25:30 +0530

    Week 2: Implemented Pod Scheduler, Health Monitor, Heartbeat System

--- api_server/app.py
+++ api_server/app.py
@@ -1,9 +1,13 @@
 # api_server/app.py
 from flask import Flask, request, jsonify
 from node_manager import NodeManager
+from scheduler import Scheduler
+from health_monitor import HealthMonitor
 
 app = Flask(__name__)
 node_manager = NodeManager()
+scheduler = Scheduler(node_manager)
+monitor = HealthMonitor(node_manager)
 
 @app.route('/add_node', methods=['POST'])
 def add_node():
@@ -11,6 +15,19 @@ def add_node():
     cpu_cores = data.get('cpu_cores')
     node_id = node_manager.register_node(cpu_cores)
     return jsonify({"message": "Node added", "node_id": node_id})
+
+@app.route('/launch_pod', methods=['POST'])
+def launch_pod():
+    data = request.json
+    cpu_req = data.get('cpu')
+    pod_id = scheduler.schedule_pod(cpu_req)
+    return jsonify({"pod_id": pod_id})
+
+@app.route('/heartbeat', methods=['POST'])
+def heartbeat():
+    data = request.json
+    monitor.receive_heartbeat(data["node_id"])
+    return jsonify({"message": "Heartbeat received"})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
--- api_server/scheduler.py
+++ api_server/scheduler.py
@@ -1,1 +1,15 @@
- 
+ # api_server/scheduler.py
+import uuid
+
+class Scheduler:
+    def __init__(self, node_manager):
+        self.node_manager = node_manager
+
+    def schedule_pod(self, cpu_req):
+        for node_id, node in self.node_manager.nodes.items():
+            if node["available_cpu"] >= cpu_req and node["healthy"]:
+                pod_id = str(uuid.uuid4())
+                node["pods"].append(pod_id)
+                node["available_cpu"] -= cpu_req
+                return pod_id
+        return "No available node"
